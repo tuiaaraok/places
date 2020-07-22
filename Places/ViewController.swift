f1d07be07a8c5f6f3457ab8e625be6650d0fde75
Tuiaara <tuyara_ok@mail.ru>
2020-07-22 10:56:35 +0300

    Initial Commit

---
+++ Places/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Places
+//
+//  Created by Айсен Шишигин on 22/07/2020.
+//  Copyright © 2020 Туйаара Оконешникова. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
